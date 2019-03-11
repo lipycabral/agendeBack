8fa83b0243f930d65b2762b49fea6912bb270e92
Felipy Cabral <lipycabral@gmail.com>
2019-03-11 15:54:00 -0500

    alt

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 const app  = express()
-const port = process.env.PORT || 3000,
+const port = process.env.PORT || 3000
 
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
